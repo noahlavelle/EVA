5cd111a69a4f9eea8327eb0a5c7bb40491d99907
Daniel Howard <danielnhoward93@gmail.com>
2020-07-25 17:40:36 +0100

    Added functionality to the rock paper scissors game

--- commands/game-status.js
+++ commands/game-status.js
@@ -4,13 +4,21 @@ module.exports = {
             game_stage : 0,
             game_playing : game,
             player_one : player_one,
-            player_two : player_two
+            player_two : player_two,
+            player_one_inputed : false,
+            player_two_inputed : false,
+            player_one_input_code : false,
+            player_two_input_code : false
         }
         module.exports.games[player_two] = {
             game_stage : 0,
             game_playing : game,
             player_one : player_one,
-            player_two : player_two
+            player_two : player_two,
+            player_one_inputed : false,
+            player_two_inputed : false,
+            player_one_input_code : false,
+            player_two_input_code : false
         }
     },
     'set_player_one' : function(variable, content, player_one) {
@@ -31,41 +39,82 @@ module.exports = {
                 module.exports.games[player_one].player_two = content
                 module.exports.games[module.exports.games[player_one].player_two].player_two = content
                 break;
+            case 'player_one_inputed':
+                module.exports.games[player_one].player_one_inputed = content
+                module.exports.games[module.exports.games[player_one].player_two].player_one_inputed = content
+                break;
+            case 'player_two_inputed':
+                module.exports.games[player_one].player_two_inputed = content
+                module.exports.games[module.exports.games[player_one].player_two].player_two_inputed = content
+                break;
+            case 'player_one_input_code':
+                module.exports.games[player_one].player_one_input_code = content
+                module.exports.games[module.exports.games[player_one].player_two].player_one_input_code = content
+                break;
+            case 'player_two_input_code':
+                module.exports.games[player_one].player_two_input_code = content
+                module.exports.games[module.exports.games[player_one].player_two].player_two_input_code = content
+                break;
         }
     },
     'set_player_two' : function(variable, content, player_two) {
         switch (variable) {
             case 'game_stage':
                 module.exports.games[player_two].game_stage = content
-                module.exports.games[module.exports.games[player_two].player_two].game_stage = content
+                module.exports.games[module.exports.games[player_two].player_one].game_stage = content
                 break;
             case 'game_playing':
                 module.exports.games[player_two].game_playing = content
-                module.exports.games[module.exports.games[player_two].player_two].game_playing = content
+                module.exports.games[module.exports.games[player_two].player_one].game_playing = content
                 break;
             case 'player_one':
                 module.exports.games[player_two].player_one = content
-                module.exports.games[module.exports.games[player_two].player_two].player_one = content
+                module.exports.games[module.exports.games[player_two].player_one].player_one = content
                 break;
             case 'player_two':
                 module.exports.games[player_two].player_two = content
-                module.exports.games[module.exports.games[player_two].player_two].player_two = content
+                module.exports.games[module.exports.games[player_two].player_one].player_two = content
+                break;
+            case 'player_one_inputed':
+                module.exports.games[player_two].player_one_inputed = content
+                module.exports.games[module.exports.games[player_two].player_one].player_one_inputed = content
+                break;
+            case 'player_two_inputed':
+                module.exports.games[player_two].player_two_inputed = content
+                module.exports.games[module.exports.games[player_two].player_one].player_two_inputed = content
+                break;
+            case 'player_one_input_code':
+                module.exports.games[player_two].player_one_input_code = content
+                module.exports.games[module.exports.games[player_two].player_one].player_one_input_code = content
+                break;
+            case 'player_two_input_code':
+                module.exports.games[player_two].player_two_input_code = content
+                module.exports.games[module.exports.games[player_two].player_one].player_two_input_code = content
                 break;
         }
     },
     'reset' : function (player_one, player_two) {
-        module.exports.games = [1];
         module.exports.games[player_one] = {
             game_stage : undefined,
-            game_playing : undefined,
+            game_playing : false,
             player_one : undefined,
-            player_two : undefined
+            player_two : undefined,
+            player_one_inputed : undefined,
+            player_two_inputed : undefined,
+            player_one_input_code : undefined,
+            player_two_input_code : undefined
         }
         module.exports.games[player_two] = {
             game_stage : undefined,
-            game_playing : undefined,
+            game_playing : false,
             player_one : undefined,
-            player_two : undefined
+            player_two : undefined,
+            player_one_inputed : undefined,
+            player_two_inputed : undefined,
+            player_one_input_code : undefined,
+            player_two_input_code : undefined
         }
     }
 }
+
+module.exports.games = [1]
--- commands/game.js
+++ commands/game.js
@@ -1,11 +1,14 @@
 const discord = require('discord.js');
+const {
+    prefix
+} = require('../config.json');
 
 module.exports = {
 	name: 'game',
     description: 'Play a game.',
     args: true,
     usage: '<user> <game>',
-    aliases: ['play'],
+    aliases: ['play', 'games', 'challenge'],
     guildOnly: true,
 	execute(message, args, client) {
         switch (args[1]) {
@@ -13,12 +16,11 @@ module.exports = {
                 let game_status = require('./game-status.js')
                 game_status.add(message.author.id, args[0].replace(/[^0-9]/g , ''), 'rps')
                 game_status = require('./game-status.js')
-                console.log(game_status.games)
                 rps_embed = new discord.MessageEmbed()
                 .setColor('#00ff00')
                 .setTitle('You have been challenged to a game of rock paper scissors by ' + message.author.username)
                 .addFields(
-                    {name: 'Please enter !rps accept or !rps decline', value: 'You have a minute to respond'}
+                    {name: 'Please enter ' + prefix + 'rps accept or ' + prefix + 'rps decline', value: 'You have a minute to respond'}
                 )
                 .setTimestamp()
                 .setThumbnail('https://www.netclipart.com/pp/m/290-2901471_rock-paper-scissors-clipart.png')
