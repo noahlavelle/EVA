ddac4e5cd89a93b872254c764eeb1a3e19f98b79
NoahLavelle <noah.lavelle@gmail.com>
2020-07-27 13:53:05 +0100

    Fixed clear error message

--- commands/staff/clear.js
+++ commands/staff/clear.js
@@ -13,14 +13,14 @@ module.exports = {
     args: true,
     guildOnly: true,
 	execute(message, args, client) {
-        if(args.length == 1) args.push(message.channel.id)
-        try {
-            client.channels.cache.get(args[1].replace(idRegex, ''))
-            .bulkDelete(parseInt(args[0]))
-            .then(message.channel.send(`Deleted ${args[0]} messages`))
-        } catch (error) {
-            message.reply(`Please enter a valid number of messages to delete.`)
+        if (args.length == 1) args.push(message.channel.id)
+        number = parseInt(args[0])
+        if (!typeof number == 'number' || number < 0 || number > 100) { 
+            return message.reply('Please enter a valid number of messages to be deleted');
         }
+        client.channels.cache.get(args[1].replace(idRegex, ''))
+        .bulkDelete(number)
+        .then(message.channel.send(`Deleted ${number} messages`))
 
 	}
 };
